5ac6cd235704add1560cde620311146be68f0934
student <student@ccm.kfupm.edu.sa>
2026-03-04 10:08:47 +0300

    todo 2

--- 5-6-react-styling-fetch-data/src/App.jsx
+++ 5-6-react-styling-fetch-data/src/App.jsx
@@ -202,9 +202,34 @@ export default function App() {
      ---------------------------------------------------------
      Implement fetch logic inside this useEffect.
      ========================================================= */
-  useEffect(() => {
-    // TODO 2.1: Implement fetching users here (see lab instructions)
-  }, []);
+   // TODO 2.1: Implement fetching users here (see lab instructions)
+
+   useEffect(() => {
+  async function fetchUsers() {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch users");
+      }
+
+      const data = await response.json();
+      setUsers(data);
+      setFilteredUsers(data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  fetchUsers();
+}, []);
 
   /* =========================================================
      TODO 2.2 — FILTER USERS BY NAME
@@ -214,6 +239,15 @@ export default function App() {
      Dependency array MUST be: [searchTerm, users]
      ========================================================= */
   useEffect(() => {
+  if (searchTerm === "") {
+    setFilteredUsers(users);
+  } else {
+    const filtered = users.filter((user) =>
+      user.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setFilteredUsers(filtered);
+  }
     // TODO 2.2: Implement filtering users here (see lab instructions)
   }, [searchTerm, users]);
 
